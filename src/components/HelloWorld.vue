f941d69b3ba0319de2fd9f2c1af071753c11841e
Cristian-Lazzari <cristian.lazzari.cl@gmail.com>
2023-09-28 13:01:36 +0200

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,29 @@
+<script>
+    export default{
+      data(){
+        return{
+          arr:[],
+          input:'',
+        }
+      },
+      methods:{
+        push(){
+          this.arr.push(this.input)
+          this.input=''
+        }
+      }
+    }
+</script>
+
+<template>
+  <h1>Lista spesa</h1>
+  <input type="text" v-model="input">
+  <button @click="push">Invia</button>
+  <ul>
+    <li v-for="element in arr" :key="element" >{{ element }}</li>
+  </ul>
+</template>
+
+<style scoped>
+
+</style>
